781200d9ce7cdf64432081a2e6be109722661e5a
Tomer Groisman <tomergroisman@gmail.com>
2021-09-12 19:51:51 +0300

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+from weather import weather
+
+# Create Flask instance
+app = Flask(__name__)
+
+
+# Check connection route
+@app.route('/health-check', methods=['GET'])
+def health_check():
+    return "Healthy"
+
+
+# weather routes
+app.register_blueprint(weather, url_prefix='/weather')
+
+#  app with a debugger
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
---
+++ src/weather/__init__.py
@@ -0,0 +1,37 @@
+from flask import Blueprint
+
+from utils import create_database, drop_database
+
+weather = Blueprint('weather', __name__)
+
+
+@weather.route('/database', methods=['POST'])
+def create_weather_database():
+    """
+    POST /weather/database
+    Create a new weather database
+
+    """
+    return create_database('weather')
+
+
+@weather.route('/database', methods=['DELETE'])
+def drop_weather_database():
+    """
+    DELETE /weather/database
+    Drop an existing weather database
+
+    """
+    return drop_database('weather')
+
+
+@weather.route('/database/<db_name>/table', methods=['DELETE'])
+def create_weather_table():
+    """
+    DELETE /weather/database
+    Drop an existing weather database
+
+    """
+    return drop_database('weather')
+
+
